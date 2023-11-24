af8118998c44732be3a293e227e6a526263b1f2f
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-24 12:52:37 +0530

    pata nahe

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
+const productController=require('../controllers/products');//up one level/controllers file/product
 
 const router = express.Router();
 
-const products = [];
+
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product',productController.getAddProduct );// don't execute it just pass the reference 
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports=router
