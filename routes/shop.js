85b7a9dfa1e960dc83d06f86d3c95bd4acae126e
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-25 15:21:40 +0530

    this one too (routes)

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
-// const adminData = require('./admin');
-const productController=require("../controllers/products")
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct)
+//the ":" signals the express to not look for a particular routes but instead the the "productId" can be anything
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
