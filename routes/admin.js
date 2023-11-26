b8b5636f94f4612aad41f82ba7cfefac24390594
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-26 15:56:13 +0530

    dynamic routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
-const productController=require('../controllers/products');//up one level/controllers file/product
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
-
 // /admin/add-product => GET
-router.get('/add-product',productController.getAddProduct );// don't execute it just pass the reference 
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
-module.exports=router
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+module.exports = router;
