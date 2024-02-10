7072eedee2158b8c1c3d2a76bce1f5e6f65a7435
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-02-10 17:17:33 +0530

    product on mongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
+
+let _db;
+
+const mongoConnect=(callback)=>{
+    MongoClient.connect('mongodb+srv://gauravyadav199623:2W9IkSwoc34JvuL3@cluster0.arquneu.mongodb.net/shop?retryWrites=true&w=majority')
+
+  .then(client=>{
+    console.log("connected!!");
+    _db=client.db() //storing connection to the database in _db
+    callback()
+  })
+  .catch(err=>{
+    console.log(err);
+    throw err
+  });
+};
+
+
+const getDb=()=>{ //return access to that data base
+  if(_db){
+    return _db
+  }
+  throw "no database found"
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
+
