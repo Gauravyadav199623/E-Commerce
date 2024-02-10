e3860f126fd79abd4df7519998f65d8766f5d7e2
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-02-10 23:11:36 +0530

    crud2

--- models/product.js
+++ models/product.js
@@ -6,17 +6,29 @@ const getDb=require('../util/database').getDb
 
 
 class Product{
-  constructor(title,price,description,imageUrl){
+  constructor(title,price,description,imageUrl,id){
     this.title=title;
     this.price=price;
     this.description=description;
-    this.imageUrl=imageUrl
+    this.imageUrl=imageUrl;
+    this._id= id ?new mongodb.ObjectId(id): null;
   }
 
   save(){
     const db=getDb();
-    return db.collection('products')
-    .insertOne(this)
+    let dbOp
+    if(this._id){
+      //update the product
+      dbOp=db
+      .collection('products')
+      .updateOne({_id:this._id},{$set:this})
+       
+    }else{
+      dbOp=db
+      .collection('products')
+      .insertOne(this)
+    }
+    return dbOp
     .then(result => {
       console.log(result);
     }).catch((err) => {
@@ -45,6 +57,18 @@ return db.collection('products').find({_id:new mongodb.ObjectId(prodId)}).next()
 .catch((err) => {
   console.log(err);
 });
+  }
+
+  static deleteById(prodId){
+    const db=getDb();
+    return db
+    .collection('products')
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then(result => {
+      console.log("deleted!");
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 }
 
